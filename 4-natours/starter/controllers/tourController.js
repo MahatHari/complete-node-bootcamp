569fee5931661ed8ded958fa7c935223702fa8ea
Hari Mahat <hmahat@gmail.com>
2021-07-30 21:08:59 +0300

    done with expres part

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -69,12 +69,13 @@ exports.updateTour = (req, res) => {
 exports.createTour = (req, res) => {
   //console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  const newTour = { id: newId, ...req.body };
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    () => {
       res.status(201).json({
         status: 'sucess',
         data: {
